971a091eb89bc0bed4f66309d423ec587d33374b
Thiago Jorge <thiago.v.jorge@hotmail.com>
2022-02-14 09:29:31 -0300

    difinitions of class, object, states and behaviors

---
+++ OOP/01- Classe x Objeto/Casa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._01__Classe_x_Objeto
+{
+    public class Casa
+    {
+        public int TamanhoM2 { get; set; }
+        public int Andares { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroVagas { get; set; }
+    }
+
+    public class Objeto
+    {
+        public Objeto()
+        {
+            var casa = new Casa()
+            {
+                Andares = 2,
+                NumeroVagas = 3,
+                TamanhoM2 = 40,
+                Valor = 100000
+            };
+        }
+    }
+}
---
+++ OOP/PilaresOOP/EstadoComportamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.PilaresOOP
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) 
+                idade--;
+
+            return idade;
+        }
+    }
+
+    
+}
