5ec5f636f355653434983af39ec01da8fcf7a801
Thiago Jorge <thiago.v.jorge@hotmail.com>
2022-02-18 16:08:02 -0300

    Interface x implementations

---
+++ OOP/02- PilaresOOP/Encapsulamento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._02__PilaresOOP
+{
+    public class Encapsulamento
+    {
+        public void FazerCafe()
+        {
+            var cafeteira = new CafeteiraEspressa();
+
+            cafeteira.Ligar();
+            cafeteira.PrepararCafe();
+            // cafeteira.MoerGraos(); Isso não vai funcionar, pois é um metodo privado da classe cafeteira
+            // sendo assim a gente esconde a implementação dos metodos internos das classes e deixamos eles disponiveis em um metodo de mais alto nivel.
+        }
+    }
+}
---
+++ OOP/InterfaceVsImplementacao/Cases.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.InterfaceVsImplementacao
+{
+    public interface IRepositorio
+    {
+        void Adicionar();
+    }
+
+    public class Repositorio : IRepositorio
+    {
+        public void Adicionar()
+        {
+            // ADICIONA ITEM
+        }
+    }
+
+    public class RepositorioFake : IRepositorio
+    {
+        public void Adicionar()
+        {
+            // ADICIONA ITEM
+        }
+    }
+
+    public class UsoImplementacao
+    {
+        public void Processo()
+        {
+            var repositorio = new Repositorio();
+            repositorio.Adicionar();
+        }
+    }
+
+    public class UsoAbstracao
+    {
+        private readonly IRepositorio _repositorio;
+
+        public UsoAbstracao(IRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public void Processo()
+        {
+            _repositorio.Adicionar();
+        }
+    }
+}
